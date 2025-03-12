eb76c23d371c6638e9d692d3c4e3436b2a1b9b50
duyguerogluu <erogluduyguduygu@gmail.com>
2025-03-12 16:58:45 +0300

    home page created

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfileScreen extends ConsumerWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profil'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Home Page'),
+          ],
+        ),
+      ),
+    );
+  }
+}
