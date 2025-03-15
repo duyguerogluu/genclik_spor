f3ad4b8b851e1ff269a0c37cb5ef192622afe23c
duyguerogluu <erogluduyguduygu@gmail.com>
2025-03-16 01:16:00 +0300

    custom drawer updated

--- lib/screens/home/components/custom_drawer.dart
+++ lib/screens/home/components/custom_drawer.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:genclik_spor/utils/colors.dart';
+import 'package:genclik_spor/utils/extensions.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: context.isDark ? offdarkblue1 : white,
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          const DrawerHeader(
-            child: Text('Başlık', style: TextStyle(color: Colors.white)),
+          DrawerHeader(
             decoration: BoxDecoration(
-              color: Color.fromRGBO(143, 148, 251, 1),
+              color: offdarkblue,
+            ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.1,
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                semanticsLabel: 'Genclik Spor Bakanligi Photo',
+                color: white,
+              ),
             ),
           ),
           ListTile(
