5e14df8622a35b0783d51f25a10796c732355ed6
duyguerogluu <erogluduyguduygu@gmail.com>
2025-03-15 23:42:59 +0300

    tabbar added

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,21 +3,44 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:genclik_spor/screens/home/components/custom_app_bar.dart';
 import 'package:genclik_spor/screens/home/components/custom_bottom_bar.dart';
 import 'package:genclik_spor/screens/home/components/custom_drawer.dart';
+import 'package:genclik_spor/utils/colors.dart';
+import 'package:genclik_spor/utils/extensions.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: CustomAppBar(),
-      drawer: CustomDrawer(),
-      bottomNavigationBar: CustomBottomBar(),
-      body: const Center(
-        child: Column(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: context.isDark ? offdarkblue : white,
+        appBar: CustomAppBar(),
+        drawer: CustomDrawer(),
+        bottomNavigationBar: CustomBottomBar(),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Home Page'),
+            TabBar(
+              labelColor: context.isDark ? white : offdarkblue,
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: context.isDark ? white : offdarkblue,
+              isScrollable: true,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              indicatorPadding: EdgeInsets.zero,
+              indicatorSize: TabBarIndicatorSize.label,
+              tabAlignment: TabAlignment.start,
+              dividerHeight: 0,
+              indicatorWeight: 0.2,
+              tabs: const [
+                Tab(text: "Anasayfa"),
+                Tab(text: "Antrenörler"),
+                Tab(text: "Spor Salonları"),
+              ],
+            ),
+            const Expanded(
+              child: Center(child: Text("Content ")),
+            ),
           ],
         ),
       ),
