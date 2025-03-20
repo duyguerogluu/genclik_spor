a75e3df7080aff25858bef24242c4d684a67126f
duyguerogluu <erogluduyguduygu@gmail.com>
2025-03-20 16:11:59 +0300

    updating gyms list style

--- lib/screens/common/gyms/gyms_screen.dart
+++ lib/screens/common/gyms/gyms_screen.dart
@@ -6,13 +6,71 @@ class GymsScreen extends ConsumerWidget {
   const GymsScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(198, 144, 4, 4),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.transparent),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.sports_football,
+                        size: 40, color: Colors.white),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Nevin Yanıt Spor Komplesksi',
+                      style: TextStyle(
+                        color: grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'adress adresss adress adressssss ',
+                      style: TextStyle(
+                        color: grey,
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'ADANA , Seyhan',
+                      style: TextStyle(
+                        color: grey,
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGymCard(IconData icon, String name, String adress, String city, BuildContext context) {
     return GestureDetector(
       onTap: () {},
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: Container(
           decoration: BoxDecoration(
-            color: const Color.fromARGB(199, 134, 57, 57),
+            color: const Color.fromARGB(200, 255, 255, 255),
             borderRadius: BorderRadius.circular(20),
             border: Border.all(color: Colors.transparent),
           ),
@@ -20,11 +78,10 @@ class GymsScreen extends ConsumerWidget {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const Icon(Icons.sports_football,
-                    size: 40, color: Colors.white),
+                Icon(icon, size: 40, color: Colors.white),
                 const SizedBox(height: 10),
                 Text(
-                  'Nevin Yanıt Spor Komplesksi',
+                  name,
                   style: TextStyle(
                     color: grey,
                     fontWeight: FontWeight.bold,
@@ -38,6 +95,5 @@ class GymsScreen extends ConsumerWidget {
         ),
       ),
     );
-    ;
   }
 }
