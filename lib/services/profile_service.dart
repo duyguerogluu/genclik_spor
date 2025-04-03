722b41409dbf7c04eccb60c78f6821f753d56cee
duyguerogluu <erogluduyguduygu@gmail.com>
2025-04-04 00:54:54 +0300

    true request

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
+import 'package:genclik_spor/models/member_profile_model.dart';
 import 'package:genclik_spor/utils/const.dart';
 import 'package:http/http.dart' as http;
 
 class ProfileService {
-  static Future<MemberProfile?> getProfile(String token) async {
+  static Future<MemberProfileModel?> getProfile(String token) async {
     final url = Uri.parse("$baseUrl/profile");
 
     final response = await http.get(
@@ -16,7 +17,7 @@ class ProfileService {
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      return MemberProfile.fromJson(data['member']);
+      return MemberProfileModel.fromJson(data['member']);
     } else {
       return null;
     }
