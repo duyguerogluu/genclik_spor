4faa92c51ae084d3ce1687f661532e526522e821
duyguerogluu <erogluduyguduygu@gmail.com>
2025-04-04 13:55:34 +0300

    some edit

--- lib/riverpod/profile/profile_riverpod.dart
+++ lib/riverpod/profile/profile_riverpod.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:genclik_spor/models/member_profile_model.dart';
 import 'package:genclik_spor/services/profile_service.dart';
 
 class ProfileRiverpod extends ChangeNotifier {
   MemberProfileModel? memberProfile;
   bool isLoading = false;
+  final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+   
 
   Future<void> fetchProfile(String token) async {
     if (memberProfile != null || isLoading) {
--- lib/screens/athlete/profilee/athlete_profile_screen.dart
+++ lib/screens/athlete/profilee/athlete_profile_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:genclik_spor/riverpod/riverpod_management.dart';
