292f65f0a85e1f328a7871ef1f05d48f3ea13cb0
duyguerogluu <erogluduyguduygu@gmail.com>
2025-04-06 23:24:06 +0300

    some error fixed

--- lib/riverpod/profile/profile_riverpod.dart
+++ lib/riverpod/profile/profile_riverpod.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:genclik_spor/models/athlete_model.dart';
 import 'package:genclik_spor/models/member_profile_model.dart';
 import 'package:genclik_spor/services/profile_service.dart';
 import 'package:genclik_spor/utils/holder.dart';
 
 class ProfileRiverpod extends ChangeNotifier {
   MemberProfileModel? memberProfile;
+  AthleteModel? athleteProfile;
   bool isLoading = false;
 
   Future<void> fetchProfile() async {
@@ -25,5 +27,27 @@ class ProfileRiverpod extends ChangeNotifier {
 
     isLoading = false;
     notifyListeners();
+  }
+
+
+
+  Future<bool> fetchAthleteProfile(AthleteModel profile) async {
+    debugPrint("Updating profile...!!!!!!!!!!!!!!!!!");
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      final result = await ProfileService.updateProfile(profile);
+      if (result) {
+        athleteProfile = profile;
+      }
+      return result;
+    } catch (e) {
+      debugPrint("Profile update error: $e");
+      return false;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
   }
 }
