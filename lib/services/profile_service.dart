3f18dadab9f08cda73c0d2357239bb3ff1a13d33
duyguerogluu <erogluduyguduygu@gmail.com>
2025-04-06 18:56:47 +0300

    token updated

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
+import 'package:genclik_spor/models/athlete_model.dart';
 import 'package:genclik_spor/models/member_profile_model.dart';
 import 'package:genclik_spor/utils/const.dart';
+import 'package:genclik_spor/utils/holder.dart';
 import 'package:http/http.dart' as http;
 
 class ProfileService {
-  static Future<MemberProfileModel?> getProfile(String token) async {
+  static Future<MemberProfileModel?> getProfile(String s) async {
     final url = Uri.parse("$baseUrl/profile");
+
+    final token = await DataHolder.getToken();
 
     final response = await http.get(
       url,
@@ -21,5 +25,25 @@ class ProfileService {
     } else {
       return null;
     }
+  }
+
+  static Future<bool> updateProfile(
+    AthleteModel profile,
+  ) async {
+    final url = Uri.parse("$baseUrl/athlete/profile");
+
+    final token = await DataHolder.getToken();
+
+    final response = await http.put(
+      url,
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(profile.toMap()),
+    );
+
+    return response.statusCode == 200;
   }
 }
--- lib/utils/holder.dart
+++ lib/utils/holder.dart
@@ -2,6 +2,13 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:genclik_spor/models/login_model.dart';
 
 class DataHolder {
+  DataHolder._();
+
   static LoginModel? loginModel;
   static FlutterSecureStorage? secureStorage;
+
+  static Future<String?> getToken() => secureStorage!.read(key: 'token');
+
+  static Future<void> putToken(String token) =>
+      secureStorage!.write(key: 'token', value: token);
 }
