6000017ac6250e4c708ec55239dbd35d1026012d
duyguerogluu <erogluduyguduygu@gmail.com>
2025-04-10 12:47:10 +0300

    course riverpod created

---
+++ lib/riverpod/course/course_riverpod.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:genclik_spor/models/course_model.dart';
+import 'package:genclik_spor/services/course_service.dart';
+
+class CourseRiverpod extends ChangeNotifier {
+  List<CourseModel> courses = [];
+  bool isLoading = false;
+
+  Future<void> fetchCourses() async {
+    if (isLoading) {
+      return;
+    }
+
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      courses = await CourseService.getCourses();
+    } catch (e) {
+      debugPrint("Course fetch error: $e");
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+}
